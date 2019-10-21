3574672265274897839c1201c2493695707c07b1
Klinggaard <oliver.klinggaard@gmail.com>
2019-10-21 14:28:59 +0200

    pc transfer

--- scripts/piCam.py
+++ scripts/piCam.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 import getpass as gp
-if gp.getuser()
+import cv2
+
+is_pi = False
+img_path = ""
+img = None
+if gp.getuser() == "pi":
+    import picamera
+    is_pi = True
+else:
+    img_path = "C:/Users/olive/Desktop/kawasaki.png"
+    img = cv2.imread(img_path)
+
+
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
