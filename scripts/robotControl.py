22312042c08f8db913b601a49d7b18a22e1c586a
kasperg3 <kasperg3@rocketmail.com>
2019-11-14 20:41:01 +0100

    Added more utility for moving the robot.

--- scripts/robotControl.py
+++ scripts/robotControl.py
@@ -1,6 +1,6 @@
 import rtde_control
 import rtde_receive
-import time
+import time, json
 
 #over camera/pregrasp: [-1.8969367186175745, -2.0070206127562464, -2.13478946685791, 4.114851637477539, -1.5151179472552698, -1.5469110647784632]
 #Large brick grasp: [-1.8101142088519495, -2.1542002163329066, -1.856816291809082, 4.01967112600293, -1.4238227049456995, -1.5479300657855433]
@@ -23,7 +23,18 @@ class RobotControl:
     def moveRobot(self, pose):
         self.rtde_c.moveJ(pose, self.velocity, self.acceleration)
 
+    def moveRobot(self, graspConfigString):
+        datastore = ""
+        with open("../scripts/PPP/grasp_config.json", 'r') as f:
+            datastore = json.load(f)
+
+        self.moveRobot(datastore[graspConfigString]["q"])
+
     def getQ(self):
         return self.rtde_r.getActualQ()
 
+    def stopRobot(self):
+        self.rtde_c.stopRobot()
 
+    def speedStop(self):
+        self.rtde_c.speedStop()
