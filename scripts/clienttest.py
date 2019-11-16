b7b4cd23ecd935094ae5bd562b3a9b014d42b6ce
Gabiabi <56154564+Gabiabi@users.noreply.github.com>
2019-11-16 17:54:13 +0100

    Newest db code - prepared to work with execute.py

--- scripts/clienttest.py
+++ scripts/clienttest.py
@@ -1,32 +1,57 @@
 import requests
 import json
 
-# GET all orders
-response = requests.get('http://127.0.0.1:5000/orders')
-# print all orders
-print (response.content)
-# convert from JSON to dictionary
-decoded = json.loads(response.content)
 
-# look for an order with smallest ID which is not taken, so we can process it
-try:
-    y = []
-    for x in decoded['orders']:
-        if x['status'] != 'taken':
-            y.append(x['id'])
-    minimal_id = min(y)
-except ():
-    print('JSON error')
+class MesOrder():
+    def __init__(self):
+        self.url = 'http://127.0.0.1:5000/orders'
+        self.counter = 0  # counter of processed orders
 
-print('id:' + ' ' + str(minimal_id))
+    def get_put_order(self):
+        # GET all orders
+        response = requests.get(self.url)
+        print(response.content)
+        decoded = json.loads(response.content)  # convert from JSON to dictionary
+        orderdict = {
+            "id": 0,
+            "ticket": "",
+            "red": 0,
+            "blue": 0,
+            "yellow": 0
+        }
+        # look for an order with smallest ID which is not taken, so we can process it
+        try:
+            y = []
+            for x in decoded['orders']:
+                if x['status'] != 'taken':
+                    y.append(x['id'])
+            minimal_id = min(y)
+            for xx in decoded['orders']:
+                if xx['id'] == minimal_id:
+                    orderdict["red"] = xx["red"]
+                    orderdict["blue"] = xx["blue"]
+                    orderdict["yellow"] = xx["yellow"]
+                    orderdict["id"] = minimal_id
+            print(orderdict)
+        except ():
+            print('JSON error')
 
-# PUT - update order, we process it, so it's status changes to taken after that
-statement_put = 'http://127.0.0.1:5000/orders/' + str(minimal_id)
-update_for_ticket = requests.put(statement_put)
-decoded_update = json.loads(update_for_ticket.content)
-ticket = decoded_update['ticket']
-print('ticket:' + ' ' + str(ticket))
+        print('id:' + ' ' + str(minimal_id))
+        # PUT - update order, we process it, so it's status changes to taken after that
+        statement_put = self.url + '/' + str(minimal_id)
+        update_for_ticket = requests.put(statement_put)
+        decoded_update = json.loads(update_for_ticket.content)
+        print(decoded_update)
+        orderdict["ticket"] = decoded_update['ticket']
+        print('ticket:' + ' ' + orderdict["ticket"])
+        print(orderdict)
+        return orderdict
 
-# DELETE, we 'processed' the order, so we delete it from the list
-stmndel = 'http://127.0.0.1:5000/orders/' + str(minimal_id) + '/' + str(ticket)
-responsedelete = requests.delete(stmndel)
+    def delete_order(self, processing):
+        # DELETE, we 'processed' the order, so we delete it from the list
+        id_delete = processing["id"]
+        ticket_delete = processing["ticket"]
+        stmndel = self.url + '/' + str(id_delete) + '/' + str(ticket_delete)
+        responsedelete = requests.delete(stmndel)
+        self.counter += 1
+        return self.counter
