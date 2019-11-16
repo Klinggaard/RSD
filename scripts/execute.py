3dca2eb2e1b11f894421206b70ac0e80b31e75db
Gabiabi <56154564+Gabiabi@users.noreply.github.com>
2019-11-16 17:52:48 +0100

    Execute state sequence - first version

---
+++ scripts/execute.py
@@ -0,0 +1,86 @@
+from scripts.modbus_client import Client
+from scripts.robotControl import RobotControl
+from Rest_MiR import Rest_MiR
+from scripts.clienttest import MesOrder
+
+
+modbus_client = Client(ip="raspberrypi.local", port=5020)  # The port will stay 5020
+robot = RobotControl()
+mir = Rest_MiR()
+db_orders = MesOrder()
+modbus_client.connect()
+
+for order_counter in range(4):
+    do_order = db_orders.get_put_order()
+    reds = do_order["red"]
+    blues = do_order["blue"]
+    yellows = do_order["yellow"]
+    while yellows > 0:
+        robot.moveRobot("LargeBrickGrasp")
+        # add grasping
+        robot.moveRobot("OverCameraPose")
+        verify = modbus_client.get_brick_colours()
+        if verify == 3:
+            print("Error, Modbus client / camera problem")
+            # add tossing out or going into hold state
+        if verify == 2:
+            robot.moveRobot("OverBoxConfig")
+            # add letting go of a block
+            # add different positions for different boxes
+            yellows -= 1
+        elif verify != 2:
+            print("Block has different color than expected - toss it out")
+            # add tossing out
+    while reds > 0:
+        robot.moveRobot("MediumBrickGrasp")
+        # add grasping
+        robot.moveRobot("OverCameraPose")
+        modbus_client.connect()
+        verify = modbus_client.get_brick_colours()
+        if verify == 3:
+            print("Error, Modbus client / camera problem")
+            # add tossing out or going into hold state
+        if verify == 1:
+            robot.moveRobot("OverBoxConfig")
+            # add different positions for different boxes
+            reds -= 1
+        elif verify != 1:
+            print("Block has different color than expected - toss it out")
+            # add tossing out
+    while blues > 0:
+        robot.moveRobot("MediumBrickGrasp")
+        # add grasping
+        robot.moveRobot("OverCameraPose")
+        modbus_client.connect()
+        verify = modbus_client.get_brick_colours()
+        if verify == 3:
+            print("Error, Modbus client / camera problem")
+            # add tossing out or going into hold state
+        if verify == 0:
+            robot.moveRobot("OverBoxConfig")
+            # add different positions for different boxes
+            blues -= 1
+        elif verify != 0:
+            print("Block has different color than expected - toss it out")
+            # add tossing out
+    if blues == 0 and yellows == 0 and reds == 0:
+        print("Sub-order completed")
+        db_orders.delete_order(do_order)
+    if order_counter == 2:
+        mir_mission = mir.get_mission("GoTo6")
+        mir.add_mission_to_queue(mir_mission)
+# we calculate based on how many times we delete an order, can be done in a lot of ways
+print("Number of completed orders: " + str(db_orders.counter % 4))
+'''
+while mir.read_register(1) != 1: # wait for MIR to arrive
+    mir.read_register(1)
+# add function to put boxes on MIR & flag to make sure we are done with packing
+mir.write_register(1, 0)  # MIR can go
+mir.write_register(2, 1)  # MIR go to base
+# change state to completing
+'''
+
+
+
+
+
