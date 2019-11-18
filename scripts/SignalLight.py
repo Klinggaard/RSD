a7c2c06148859c1dea5211c90bb1a66b05357cf3
evouzion <44097888+evouzion@users.noreply.github.com>
2019-11-18 13:02:47 +0100

    Add files via upload

--- scripts/SignalLight.py
+++ scripts/SignalLight.py
@@ -1,29 +1,89 @@
 import rtde_io
 import time
 import random
+import finite_state_machine.py
 
 import rtde_io
-rtde_io = rtde_io.RTDEIOInterface("192.168.0.99")
 
-print("TURNING ON THE LIGHTS")
-#Light IO
-rtde_io.setStandardDigitalOut(1, True)
-rtde_io.setStandardDigitalOut(2, True)
-rtde_io.setStandardDigitalOut(3, True)
+def lights_on_of():
+    rtde_io = rtde_io.RTDEIOInterface("192.168.0.99")
 
-#Valve IO
-rtde_io.setStandardDigitalOut(0,True)
-
-
-while(True):
-    time.sleep(1)
-    rtde_io.setStandardDigitalOut(1, False)
-    rtde_io.setStandardDigitalOut(2, False)
-    rtde_io.setStandardDigitalOut(3, False)
-    rtde_io.setStandardDigitalOut(0,True)
-    time.sleep(1)
+    print("TURNING ON THE LIGHTS")
+    #Light IO
     rtde_io.setStandardDigitalOut(1, True)
     rtde_io.setStandardDigitalOut(2, True)
     rtde_io.setStandardDigitalOut(3, True)
+
+    #Valve IO
     rtde_io.setStandardDigitalOut(0,True)
 
+
+    while(True):
+        time.sleep(1)
+        rtde_io.setStandardDigitalOut(1, False)
+        rtde_io.setStandardDigitalOut(2, False)
+        rtde_io.setStandardDigitalOut(3, False)
+        rtde_io.setStandardDigitalOut(0,True)
+        time.sleep(1)
+        rtde_io.setStandardDigitalOut(1, True)
+        rtde_io.setStandardDigitalOut(2, True)
+        rtde_io.setStandardDigitalOut(3, True)
+        rtde_io.setStandardDigitalOut(0,True)
+
+def light_tower(state):
+    if state=='Stopping' or state=='Stopped':
+        rtde_io.setStandardDigitalOut(3,True)
+        rtde_io.setStandardDigitalOut(2,False)
+        rtde_io.setStandardDigitalOut(1,False)
+    elif state=='Aborting' or state=='Aborted' or state=='Clearing':
+        while (True):
+            rtde_io.setStandardDigitalOut(3,True)
+            rtde_io.setStandardDigitalOut(2,False)
+            rtde_io.setStandardDigitalOut(1,False)
+            sleep(1)
+            rtde_io.setStandardDigitalOut(3,False)
+            rtde_io.setStandardDigitalOut(2,False)
+            rtde_io.setStandardDigitalOut(1,False)
+            sleep(1)
+    elif  state=='Resetting':
+         while True:
+            rtde_io.setStandardDigitalOut(3,False)
+            rtde_io.setStandardDigitalOut(2,True)
+            rtde_io.setStandardDigitalOut(1,False)
+            sleep(1)
+            rtde_io.setStandardDigitalOut(3,False)
+            rtde_io.setStandardDigitalOut(2,False)
+            rtde_io.setStandardDigitalOut(1,False)
+            sleep(1)
+    elif state=='Suspending' or state=='Suspended':
+        rtde_io.setStandardDigitalOut(2,True)
+        rtde_io.setStandardDigitalOut(3,False)
+        rtde_io.setStandardDigitalOut(1,False)
+    elif state=='Idle':
+         while (True):
+            rtde_io.setStandardDigitalOut(3,False)
+            rtde_io.setStandardDigitalOut(2,False)
+            rtde_io.setStandardDigitalOut(1,True)
+            sleep(1)
+            rtde_io.setStandardDigitalOut(3,False)
+            rtde_io.setStandardDigitalOut(2,False)
+            rtde_io.setStandardDigitalOut(1,False)
+            sleep(1)
+    elif state=='Starting' or state=='Executing' or state=='Unholding' or state=='Unsuspending':
+        rtde_io.setStandardDigitalOut(1,True)
+        rtde_io.setStandardDigitalOut(3,False)
+        rtde_io.setStandardDigitalOut(2,False)
+    elif state=='Holding' or state=='Held':
+         while (True):
+            rtde_io.setStandardDigitalOut(3,False)
+            rtde_io.setStandardDigitalOut(2,True)
+            rtde_io.setStandardDigitalOut(1,True)
+            sleep(1)
+            rtde_io.setStandardDigitalOut(3,False)
+            rtde_io.setStandardDigitalOut(2,False)
+            rtde_io.setStandardDigitalOut(1,False)
+            sleep(1)
+
+while (True):
+    State=FiniteStateMachine.state
+    light_tower(State)
