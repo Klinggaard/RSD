9cadd9dbbb9b4ad05c19597ac7cc48e8d468e039
Gabiabi <56154564+Gabiabi@users.noreply.github.com>
2019-11-18 19:02:00 +0100

    Update Rest_MiR.py

--- Rest_MiR.py
+++ Rest_MiR.py
@@ -1,17 +1,18 @@
 import requests
+import time
 
 #Probable will change after network is ready
-url = 'http://mir.com/api/v2.0.0/'
+url = 'http://10.10.19.40/api/v2.0.0/'
 
 Authorization = {
     'Authorization': "PUT_THE_RIGHT_KEY"}
 language = {'Accept-Language': "en_US"}
 
 
-class Rest_MiR():
+class RestMiR():
     def __init__(self):
         self.authorization = {
-            'Authorization': "PUT_THE_RIGHT_KEY"}
+            'Authorization': "Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=="}
 
     #We need to put the name of our mission
     def get_mission(self, mission_name="MISSION_NAME"):
@@ -32,7 +33,7 @@ class Rest_MiR():
         if response.status_code != 201:
             print("ERROR" + str(response.status_code))
 
-    #In the mission we will have to set coils(plc registers) in order to get information if robot has docked and etc.
+    #In the mission we will have to set coils (plc registers) in order to get information if robot has docked and etc.
     def read_register(self, register_id):
         response = requests.get(url + 'registers/' + str(register_id), headers=self.authorization)
         register_value = 0
@@ -52,3 +53,16 @@ class Rest_MiR():
         if response.status_code != 200:
             print(response.status_code)
         return 0
+
+
+mir = RestMiR()
+guid = mir.get_mission("GoTo6")
+mir.add_mission_to_queue(guid)
+while mir.read_register(6) != 1: # wait for MIR to arrive
+    mir.read_register(1)
+# add function to put boxes on MIR & flag to make sure we are done with packing
+print("mir arrived")
+time.sleep(2)
+mir.write_register(6, 0)  # MIR can go
+print("bye MIR")
+# change state to completing
